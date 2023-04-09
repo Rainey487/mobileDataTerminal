1a5e4a6e371ab7475df513000a2ac501a6152301
Rainey487 <109980223+Rainey487@users.noreply.github.com>
2023-04-09 17:26:32 +1000

    Firebase added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from 'firebase/app';
 
 const firebaseConfig = {
     apiKey: "AIzaSyA0NjB60bOcTWz1DDFt-pbR5pzyTZxzLnY",
@@ -9,4 +11,24 @@ const firebaseConfig = {
     appId: "1:823891967200:web:252c129429b46d0b206485"
 };
 
-const app = initializeApp(firebaseConfig); 
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+document.querySelector('#signUpForm').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const email = document.querySelector('#signUpForm [name="email"]').value;
+    const password = document.querySelector('#signUpForm [name="password"]').value;
+    auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // User created successfully
+            const user = userCredential.user;
+            alert('User created successfully!');
+            // TODO: Redirect to a success page or sign the user in
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(`Error: ${errorMessage}`);
+        });
+});
