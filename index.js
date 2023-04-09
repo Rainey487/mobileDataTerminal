753cdcb443659528445951c23b79a0614505e109
Rainey487 <109980223+Rainey487@users.noreply.github.com>
2023-04-09 19:08:51 +1000

    Firebase signup added

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    // Your Firebase configuration here
+    apiKey: "AIzaSyA0NjB60bOcTWz1DDFt-pbR5pzyTZxzLnY",
+    authDomain: "roleplay-community.firebaseapp.com",
+    projectId: "roleplay-community",
+    storageBucket: "roleplay-community.appspot.com",
+    messagingSenderId: "823891967200",
+    appId: "1:823891967200:web:252c129429b46d0b206485"
 };
 
 // Initialize Firebase
