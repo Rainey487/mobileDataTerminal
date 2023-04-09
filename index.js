bcc9400cdbb2e76800d24958795e74d353067798
Rainey487 <109980223+Rainey487@users.noreply.github.com>
2023-04-09 19:02:58 +1000

    Firebase signup added

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { getFirestore } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0NjB60bOcTWz1DDFt-pbR5pzyTZxzLnY",
-    authDomain: "roleplay-community.firebaseapp.com",
-    projectId: "roleplay-community",
-    storageBucket: "roleplay-community.appspot.com",
-    messagingSenderId: "823891967200",
-    appId: "1:823891967200:web:252c129429b46d0b206485"
+    // Your Firebase configuration here
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-const db = getFirestore(app)
-
-
-const newSignUp = (email, password, identifier) => {
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            const uid = user.uid;
-            firebase.firestore().collection('users').doc(uid).set({
-                identifier: identifier,
-                email: email
-            });
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error(errorCode, errorMessage);
-        });
-}
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 const signUpForm = document.getElementById('signUpForm');
+const emailInput = signUpForm.email;
+const passwordInput = signUpForm.password;
+const identifierInput = signUpForm.identifier;
+
+const signUp = async (email, password, identifier) => {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const uid = userCredential.user.uid;
+        const userDocRef = doc(collection(db, 'users'), uid);
+        await setDoc(userDocRef, {
+            email: email,
+            identifier: identifier
+        });
+        console.log('User created with uid:', uid);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 signUpForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    const email = signUpForm.email.value;
-    const password = signUpForm.password.value;
-    const identifier = signUpForm.identifier.value;
-    newSignUp(email, password, identifier);
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const identifier = identifierInput.value;
+    signUp(email, password, identifier);
 });
